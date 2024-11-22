a10232cf41b74c49dabec1ed1cc0b47f7f4d8f71
Brunasantitos <santitos@outlook.com.br>
2024-11-21 23:59:33 -0300

    entendo a utilização do formatter e atribuindo valores diferenes nas variáveis

---
+++ 5-atividade/formating_variables.rb
@@ -0,0 +1,15 @@
+# a utilização do #{} é para formatar strings
+# a utilização do %{} é para aplicar o mesmo formato a vários valores
+formatter = "%{first} %{second} %{third} %{fourth}"
+
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+puts formatter % {first: "onde", second: "two", third: "three", fourth: "four"}
+puts formatter % {first: true, second: false, third: true, fourth: false}
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+
+puts formatter % {
+  first: "I had this thing.",
+  second: "That you could type up right.",
+  third: "But it didn't sing.",
+  fourth: "So I said goodnight."
+}
