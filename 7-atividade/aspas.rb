f4fd96877a06c252de1e580d4f6039679751b849
Brunasantitos <santitos@outlook.com.br>
2024-11-22 23:47:39 -0300

    criando exemplos para uso de símbolos

---
+++ 7-atividade/aspas.rb
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in." #espaço de parágrafo
+persian_cat = "I'm split\non a line." #quebra de linha
+backslash_cat = "I'm \\ a \\ cat." #imprime a barra
+
+fat_cat = """ #imprime todo mundo
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+""" #usar aspas simples, gera erro de sintaxe
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
