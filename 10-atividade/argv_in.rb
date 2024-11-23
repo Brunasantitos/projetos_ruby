52638c6441907f8a4c76b3638ed12d2ae67ca7c8
Brunasantitos <santitos@outlook.com.br>
2024-11-23 00:18:35 -0300

    usando o ARGV como outra alternativa de entrada de dados

---
+++ 10-atividade/argv_in.rb
@@ -0,0 +1,8 @@
+first, second, third = ARGV #argv variável de argumento
+#linha 1 está descompactando
+#a forma correta de rodar o programa
+#ruby argv.rb firstValor, secondValor, thirdValor
+
+puts "Your fisrt variable is: #{first}"
+puts "your second variable is #{second}"
+puts "Your third variable is: #{third}"
