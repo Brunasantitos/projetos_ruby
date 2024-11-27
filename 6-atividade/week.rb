68febcceca46fd358bca9d95ad50be57c14fde0b
Brunasantitos <santitos@outlook.com.br>
2024-11-27 20:10:28 -0300

    alteração de linha, explicação da utilização do %q no puts

--- 6-atividade/week.rb
+++ 6-atividade/week.rb
@@ -4,6 +4,7 @@ months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 puts "Here are the day: #{days}"
 puts "Here ate the months: #{months}"
 
+#%q utilizado na frente, permite imprimir várias linhas sem aspas
 puts %q{
 There's something going on here.
 With this weird quote
