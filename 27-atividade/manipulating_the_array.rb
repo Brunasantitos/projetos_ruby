8e516d0904835995d9687031e3b500606beea996
Brunasantitos <santitos@outlook.com.br>
2024-12-11 18:06:58 -0300

    manipulando array

---
+++ 27-atividade/manipulating_the_array.rb
@@ -0,0 +1,26 @@
+#Capítulo 34
+animals = [ 'bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#O animal do índice 0, é o primeiro animal
+puts animals[0]
+
+#O terceiro animal, é do índice 4
+puts animals[4]
+
+#O primeiro animal, é do índice 0
+puts animals[0]
+
+#O animal do índice 4, é o terceiro animal
+puts animals[4]
+
+#O quinto animal, é do índice 6
+puts animals[6] #não existe animal nessa lista
+
+#O animal do índice 3, é o segundo animal
+puts animals[3]
+
+#O sexto animal, é do índice 7
+puts animals[7] #não existe animal nessa lista
+
+#O animal do índice 5, é o quarto animal
+puts animals[5]
