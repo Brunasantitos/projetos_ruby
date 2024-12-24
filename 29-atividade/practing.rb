a89e4927c1c5fcb8a7bfc1009c969bdb1364c8ec
Brunasantitos <santitos@outlook.com.br>
2024-12-23 22:59:34 -0300

    é uma pasta de teste

--- 29-atividade/practing.rb
+++ 29-atividade/practing.rb
@@ -1,11 +1,8 @@
 #Capítulo 36
 #reafzer a atividade de acordo com o que é solicitado no capítulo
 
-contador = 0
 
-until contador == 5
-  puts "Contador: #{contador}"
-  contador += 1
-end
+puts "Linha 1 \vLinha 2 \vLinha 3"
+
 
   
